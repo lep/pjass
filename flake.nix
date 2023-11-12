c1fa89ab6bebdc259aa29bfeb0848f7b29bdd422
lep <lep.delete@googlemail.com>
2023-11-12 16:28:52 +0100

    Nixfmt flake.nix

--- flake.nix
+++ flake.nix
@@ -1,57 +1,47 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs";
-        flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem (system:
-            let pkgs = import nixpkgs { inherit system; };
-                packageName = "pjass";
-                buildInputs = [
-                    pkgs.gnumake
-                    pkgs.flex
-                    pkgs.bison
-                ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        packageName = "pjass";
+        buildInputs = [ pkgs.gnumake pkgs.flex pkgs.bison ];
 
-                drv-args = {
-                        name = packageName;
-                        src = self;
-                        inherit buildInputs;
+        drv-args = {
+          name = packageName;
+          src = self;
+          inherit buildInputs;
 
-                        buildPhase = "make pjass";
-                        installPhase = ''
-                            mkdir -p $out/bin
-                            install -t $out/bin pjass
-                        '';
+          buildPhase = "make pjass";
+          installPhase = ''
+            install -Dt $out/bin pjass
+          '';
+        };
+        drv = pkgs.stdenv.mkDerivation drv-args;
 
-                        FLEX = "${pkgs.flex}/bin/flex";
-                        BISON = "${pkgs.bison}/bin/bison";
-                    };
-		drv = pkgs.stdenv.mkDerivation drv-args;
+        mingw = pkgs.pkgsCross.mingw32;
+      in rec {
+        apps.default = {
+          type = "app";
+          program = "${drv}/bin/pjass";
+        };
+        apps.pjass = apps.default;
+        packages = {
+          default = drv;
+          ${packageName} = drv;
+          "${packageName}-mingw32" = mingw.stdenv.mkDerivation (drv-args // {
+            CFLAGS = "-O2";
+            installPhase = ''
+              install -Dt $out/bin pjass.exe
+            '';
+          });
+        };
 
-
-                mingw = pkgs.pkgsCross.mingw32;
-            in rec {
-		apps.default = {
-		    type = "app";
-		    program = "${drv}/bin/pjass";
-		};
-		apps.pjass = apps.default;
-                packages = {
-                    ${packageName} = drv;
-                    "${packageName}-mingw32" = mingw.stdenv.mkDerivation (drv-args //
-                        {
-                            CFLAGS = "-O2";
-                            installPhase = ''
-                                mkdir -p $out/bin
-                                install -t $out/bin pjass.exe
-                            '';
-                        });
-                };
-
-                defaultPackage = packages.${packageName};
-            }
-        );
+        defaultPackage = packages.${packageName};
+      });
 }
-    
+
